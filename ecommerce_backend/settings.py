7a8aed1fe537c3014776773ec42c2fdb033ba83b
Franco-Micheletti <79060860+Franco-Micheletti@users.noreply.github.com>
2023-04-07 18:35:23 -0300

    removed database local

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -87,24 +87,24 @@ WSGI_APPLICATION = 'ecommerce_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # DATABASES = {
 #     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {'charset': 'utf8mb4',
-                    'use_unicode': True}
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#         'OPTIONS': {'charset': 'utf8mb4',
+#                     'use_unicode': True}
+#     }
+# }
 
 AUTH_USER_MODEL = 'login.CustomUser'
 
