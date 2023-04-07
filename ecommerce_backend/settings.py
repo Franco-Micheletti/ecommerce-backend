91b6e96c5f15ba007f56fd7022f28c5b0e8a82a7
Franco-Micheletti <79060860+Franco-Micheletti@users.noreply.github.com>
2023-04-07 18:54:08 -0300

    dwquihdw

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -87,11 +87,14 @@ WSGI_APPLICATION = 'ecommerce_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASE_URL = os.getenv("DATABASE_URL")
+# ------------------------ FOR DEVELOPMENT AT EXTERNAL DATABASE ------------------
 
-# DATABASES = {
-#     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-# }
+DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
+
+#------------------------ FOR DEVELOPMENT AT LOCAL DATABASE ----------------------
 
 # DATABASES = {
 #     'default': {
@@ -105,6 +108,7 @@ WSGI_APPLICATION = 'ecommerce_backend.wsgi.application'
 #                     'use_unicode': True}
 #     }
 # }
+# -------------------------------------------------------------------------------
 
 AUTH_USER_MODEL = 'login.CustomUser'
 
